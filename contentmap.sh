e0d7f5c42f00b9e918c9578d1a6ba18acfa3c160
Chris Hughes <chris@hughes-net.net>
2015-07-22 11:48:11 -0400

    removed s from version
    There was a trailing s on the TS version number so I removed it via Sed.

--- contentmap.sh
+++ contentmap.sh
@@ -6,7 +6,7 @@
 # Edited On:	22 July 2015 
 
 
-grep v\= access.log | grep -v "\"\-\"" | awk '{print $7, $11, $NF}' | awk -FS"?v=" '{print $1,"\t", $2}' | awk -F'[&]' '{print  $1 $2 $3 $NF}' | awk '{print $2 $3 $NF}' | awk '{print $2 $3 $NF}' | sed 's/\=/ /g' | sed 's/\"/ /g' |  grep v\= access.log | grep -v "\"\-\"" | awk '{print $7, $11, $NF}' | awk -FS"?v=" '{print $1,"\t", $2}' | awk -F'[&]' '{print  $1 $2 $3 $NF}' | awk '{print $2 $3 $NF}' | awk '{print $2 $3 $NF}' | sed 's/\=/ /g' | sed 's/\"/ /g' | sed 's/\// /g' |  awk -F\/ '{print $1 $2 $3 $NF}' | sed 's/https://g' | awk '{print $1","$5","$NF}' | grep -v errata | sort -u > `date +"%Y%m%d_%H%M%S"`_contentmap.csv
+grep v\= access.log | grep -v "\"\-\"" | awk '{print $7, $11, $NF}' | awk -FS"?v=" '{print $1,"\t", $2}' | awk -F'[&]' '{print  $1 $2 $3 $NF}' | awk '{print $2 $3 $NF}' | awk '{print $2 $3 $NF}' | sed 's/\=/ /g' | sed 's/\"/ /g' |  grep v\= access.log | grep -v "\"\-\"" | awk '{print $7, $11, $NF}' | awk -FS"?v=" '{print $1,"\t", $2}' | awk -F'[&]' '{print  $1 $2 $3 $NF}' | awk '{print $2 $3 $NF}' | awk '{print $2 $3 $NF}' | sed 's/\=/ /g' | sed 's/\"/ /g' | sed 's/\// /g' |  awk -F\/ '{print $1 $2 $3 $NF}' | sed 's/https://g' | awk '{print $1","$5","$NF}' | grep -v errata | sed 's/s//' | sort -u > `date +"%Y%m%d_%H%M%S"`_contentmap.csv
 
 exit
 
