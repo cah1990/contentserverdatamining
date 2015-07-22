f73a2b279f96f20b2c7b40a0d7030f5e2be97e1e
Chris Hughes <chris@hughes-net.net>
2015-07-22 11:32:41 -0400

    updates
    Removed CDN field and other superfluous info.

--- contentmap.sh
+++ contentmap.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+# contentmap.sh
 # By:   	Chris Hughes
 # On:		8 July 2015
 # Edited By: 	Chris Hughes
-# Edited On:	21 July 2015 - Removed "s from results
+# Edited On:	22 July 2015 
 
-grep  v\= access.log |  awk '{print $1, $7, $11, $NF}' | awk -FS"?v=" '{print $1,"\t", $2}' >> stage1.txt
-cat stage1.txt | awk -F'[&]' '{print $1, $2, $NF}' | awk '{print $1,",", $3,",", $6,",", $NF}' >> mappedcontent.csv
-sed 's/\"//g' mappedcontent.csv
-rm -f stage1.txt
+
+grep v\= access.log | grep -v "\"\-\"" | awk '{print $7, $11, $NF}' | awk -FS"?v=" '{print $1,"\t", $2}' | awk -F'[&]' '{print  $1 $2 $3 $NF}' | awk '{print $2 $3 $NF}' | awk '{print $2 $3 $NF}' | sed 's/\=/ /g' | sed 's/\"/ /g' |  grep v\= access.log | grep -v "\"\-\"" | awk '{print $7, $11, $NF}' | awk -FS"?v=" '{print $1,"\t", $2}' | awk -F'[&]' '{print  $1 $2 $3 $NF}' | awk '{print $2 $3 $NF}' | awk '{print $2 $3 $NF}' | sed 's/\=/ /g' | sed 's/\"/ /g' | sed 's/\// /g' |  awk -F\/ '{print $1 $2 $3 $NF}' | sed 's/https://g' | awk '{print $1","$5","$NF}' | grep -v errata | sort -u > `date +"%Y%m%d_%H%M%S"`_contentmap.csv
+
+exit
+
--- solutionmap.sh
+++ solutionmap.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# solutionmap.sh
 # By:   	Chris Hughes
 # On:		21 July 2015
 # Edited By:
@@ -6,15 +7,6 @@
 # Pulls the Solutions Bundle and requesting IP from the Content Server access logs.
 # Requests that don't contain data in each of these fields are dropped.  This list also shows only unique results.
 
-#rm -f solstage1.txt solstage2.txt solstage3.txt mappedsolutions.csv
-#grep "GET /files/published" access.log |  awk '{print $7, $NF}' > solstage1.txt
-#cat solstage1.txt | awk -F/ '{print $4, $NF }' | awk '{print $1, $NF}'| uniq > solstage2.txt
-#sed 's/\"//g' solstage2.txt > solstage3.txt
-#cat solstage3.txt |grep -v "-" | awk '{print $1,",",$2}'| sort -u  > mappedsolutions.csv
-#rm -f solstage1.txt solstage2.txt
-
-grep "GET /files/published/"  access.log | awk '{print $1,$7, $11, $NF}' | grep -v "\"\-\"" | awk -F/ '{print $1","$4","$8","$9}' | sed 's/\"//g' | sort -u > mappedsolutions.csv
-
-
-
+grep "GET /files/published/"  access.log | awk '{print $7, $11, $NF}'| grep -v "\"\-\"" | sed 's/\"//g' | sed 's/https:\/\// /g' | sed 's/\// /g' | awk '{print $3","$6","$NF}'| grep -v errata | sort -u > `date +"%Y%m%d_%H%M%S"`_solutionmap.csv
+ 
 exit
